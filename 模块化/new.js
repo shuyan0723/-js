fb5f8c1fbb6cb7f304cecef1574f42915758e3a4
shuyan0723 <2737559680@qq.com>
2025-08-21 10:24:44 +0800

    es6模块化

---
+++ 模块化/new.js
@@ -0,0 +1,12 @@
+console.log('new.....');
+
+import { add } from './add.js';
+import now from './now.js';
+
+
+
+const n=add(1,2);
+console.log('n',n);
+console.log('now',now());
+//webpack 传统打包
+//vite
